d951884b4565c19ae54f52c14a5719393f90b69d
Zahraa <zahraahawie@gmail.com>
2022-09-25 20:36:47 +0300

    First commit

---
+++ src/components/Meme.js
@@ -0,0 +1,65 @@
+import React from "react";
+
+export default function Meme() {
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+  const [allMemes, setAllMemes] = React.useState([]);
+
+  function getMemeImage() {
+    const randomNumber = Math.floor(Math.random() * allMemes.length);
+    const url = allMemes[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
+  }
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => {
+      return {
+        ...prevMeme,
+        [name]: value,
+      };
+    });
+  }
+
+  React.useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setAllMemes(data.data.memes));
+  }, []);
+
+  return (
+    <main>
+      <div className="form">
+        <input
+          type="text"
+          placeholder="Top text"
+          className="form--input"
+          onChange={handleChange}
+          name="topText"
+          value={meme.topText}
+        />
+        <input
+          type="text"
+          placeholder="Bottom text"
+          className="form--input"
+          onChange={handleChange}
+          name="bottomText"
+          value={meme.bottomText}
+        />
+        <button onClick={getMemeImage} className="form--button">
+          Get a new meme image 🖼
+        </button>
+        <div className="meme">
+          <img src={meme.randomImage} alt="MemeImage" className="meme--image" />
+          <h2 className="meme--text top">{meme.topText}</h2>
+          <h2 className="meme--text bottom">{meme.bottomText}</h2>
+        </div>
+      </div>
+    </main>
+  );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <img src="./images/Troll.png" alt="ok" className="nav--logo" />
+      <h2 className="nav--title"> Meme Generator </h2>
+      <h3 className="nav--description"> React Course - Project 3</h3>
+    </nav>
+  );
+}
